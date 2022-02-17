b408117d7ccb6f3e06ce814c79d686e6cf271ba4
Simarjeet Kaur <simarjeet518h@gmail.com>
2022-02-16 19:49:48 -0800

    minor changes

--- src/routes/budget.js
+++ src/routes/budget.js
@@ -5,10 +5,9 @@ module.exports = (db) => {
 
   router.post("/:id", (req, res) => {
     const id = req.body.id;
-    const user_id = req.body.user_id;
     const budget = req.body.budget;
   db.query(`INSERT INTO users_monthly_allowance(id,user_id, budget) VALUES ($1, $2, $3);
-  `,[id, user_id, budget])
+  `,[id,req.params.id, budget])
   .then((data) => {
     res.sendStatus(200);
   })
@@ -21,7 +20,7 @@ module.exports = (db) => {
     let date  = new Date();
     let currentMonthStartDate  = (new Date(date.getFullYear(), date.getMonth(), 1));
     let currentMonthLastDate  = (new Date(date.getFullYear(), date.getMonth()+1, 0));
-    db.query('SELECT id, budget,date  FROM users_monthly_allowance WHERE user_id =$1 ', [req.params.id])
+    db.query('SELECT id, budget,date  FROM users_monthly_allowance WHERE id=$1 ', [8])
       .then((data) => {
         res.json(data.rows);
       })
